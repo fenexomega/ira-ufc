77b79e1c1530c250e01fedcdaad645120dfa2a40
Jordy <jordyfgomes@gmail.com>
2016-12-20 09:15:09 -0300

    services

---
+++ js/services/iraService.js
@@ -0,0 +1,15 @@
+(function(){
+
+  'use strict';
+  var services = angular.module('mainApp.services');
+
+  function IraService($http,config){
+    var ENVIAR_DADOS = UrlGetter.address(routes.ENVIAR_DADOS);
+
+    var IraService = {};
+
+    function 
+
+  };
+
+})();
